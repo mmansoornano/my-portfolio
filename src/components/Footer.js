31246f82964753859e9569fe1dc8dde4e4b0bcfc
home <mmansoor.nano@gmail.com>
2022-08-26 13:08:29 +0500

    about

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,10 +14,10 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3></h3>
+          <h3>{' '}</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3></h3>
+          <h3>{' '}</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
